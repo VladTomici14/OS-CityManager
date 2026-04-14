501f28bae8acd32b9d2924eb7703a08c3322b42b
VladTomici14 <vladtomici@gmail.com>
2026-04-14 09:20:07 +0300

    added the general city manager header

---
+++ include/city_manager.h
@@ -0,0 +1,26 @@
+#ifndef CITY_MANAGER_H
+#define CITY_MANAGER_H
+
+#include <time.h>
+
+#define MAX_STR_LEN 64
+#define DESC_LEN 256
+
+typedef struct {
+    int report_id;
+    char inspector_name[MAX_STR_LEN];
+    double latitude;
+    double longitude;
+    char category[MAX_STR_LEN];
+    int severity;
+    time_t timestamp;
+    char description[DESC_LEN];
+} Report;
+
+// ----- filter methods -----
+int parse_condition(const char *input, char *field, char *op, char *value);
+int match_condition(Report *r, const char *field, const char *op, const char *value);
+
+void execute_filter(const char *district_id, const char *role, int argc, char *argv[], int start_idx);
+
+#endif
